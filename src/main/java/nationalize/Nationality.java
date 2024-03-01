1e1b9348f7c2ba63f7573b1c8684a7df4f97ac2f
PaliczLaptop <hdtoki@gmail.com>
2024-03-01 01:25:56 +0100

    perf!: change Nationality to a record class

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,24 +1,9 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
-import lombok.Data;
 
 import java.util.List;
 
-@Data
-public class Nationality {
-
-    private long count;
-    private String name;
-
-    @SerializedName("country")
-    private List<Country> countries;
-
-    @Data
-    public static class Country {
-
-        private String countryId;
-        private float probability;
-    }
-
-}
+public record Nationality(long count, String name, @SerializedName("country") List<Country> countries) {
+public record Country(String countryId, float probability) {}
+        }
